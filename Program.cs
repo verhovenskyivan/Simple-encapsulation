45c1ba9841427e997bb035b9064cde68a0e87d9f
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2022-12-03 19:29:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Encapsulated
+{
+    private double valueDouble;
+    private string valueString;
+
+
+    public double ValueDouble
+    {
+        get { return valueDouble; }
+        set
+        {
+            valueDouble = value;
+            valueString = value.ToString();
+        }
+    }
+
+    public string ValueString
+    {
+        get { return valueString; }
+        set {
+            
+            valueString = value.ToString();
+        }
+    }
+}
